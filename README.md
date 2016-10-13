edadcf971fea1f3e0d852cf8ad8126328dfd8835
Levin Du <guest@the520.cn>
2016-10-13 17:11:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# A SIMPLE minirouter for spa
+使用原生API，十分简短，支持链式语法，使用简洁优雅。
+
+## API:
+`Router.use(path, controller)`
+
+path: 即路径
+controller: 一个包含的render等方法的对象. 
+
+```javascript
+var mockController = {
+    render: function(){
+        return "<div></div>"
+    }
+    onNavigateTo: function(){
+        //this对象为当前组件，即{path, controller: controller}对象
+    }
+    onLeave: function(){
+
+    }
+}
+```
+
+`Router.default`
+
+设置默认路径
+
+`Router.run`
+
+bootstrap整个路由
+
+
+## DEMO:
+```javascript
+var router = new Router('#app');
+router.use('/', {
+    render: function(){
+        return '<div><h4>page1</h4><p><a href="#/baidu">a path does not exits<a><br/><a href="#/page2">#page2</a></p></div>';
+    },
+    onNavigateTo: function(){
+        alert("hi i\'m here, page1");
+    },
+    onLeave: function(){
+        alert('goodbye page1');
+    }
+}).use('/page2', {
+    render: function(){
+        return '<div>page2</div>';
+    },
+    onNavigateTo: function(){
+        alert('hi page2');
+    },
+    onLeave: function(){
+        alert('bye page2');
+    }
+}).run();
+``` 
+
+## 说明
+
+MIT Licence.
+
---
+++ minirouter.js
@@ -0,0 +1,94 @@
+
+/*
+var mockController = {
+    render: function(){
+        return "<div></div>"
+    }
+    onNavigateTo: function(){
+        //this对象为当前组件，即{path, controller: controller}对象
+    }
+    onLeave: function(){
+
+    }
+}
+ */
+(function (window) {
+    function getRoute(routerObj, path, defaultPath) {
+        var key = (path.hash || '#/').slice(1).split('?')[0];
+        if (routerObj.hasOwnProperty(key)) {
+            return routerObj[key];
+        }
+
+        if (path !== defaultPath && !!defaultPath) {
+            return getRoute(routerObj, defaultPath);
+        }
+    }
+
+    function getFullPath(path) {
+        if (URL.prototype.isPrototypeOf(path)) return path;
+        var link = document.createElement('a');
+        link.setAttribute('href', path);
+        return new URL(link.href);
+    }
+
+    function noop() {
+    }
+
+    var Router = function Router(container) {
+        this._container = typeof container === 'string' ? document.querySelector(container) : container;
+        this._routes = {};
+    };
+
+
+    Router.prototype.use = function use(path, controller) {
+        if (typeof (controller.render) !== 'function') {
+            throw new Error('未实现render');
+        }
+        var map = this._routes || {};
+        map[path] = {
+            path: path,
+            controller: controller
+        }
+        return this;
+    }
+
+    Router.prototype.default = function (defaultPath) {
+        this._defaultPath = defaultPath;
+    }
+
+    Router.prototype.go = function (nextPath, prev, replace) {
+        var current = getRoute(this._routes, prev || window.location),
+            next = getRoute(this._routes, getFullPath(nextPath), this._defaultPath);
+
+        current && (current.controller.onLeave || noop).call(current);
+        var html = next.controller.render();
+        if (replace) {
+            history.replaceState(document.title, document.title, location.pathname + '#' + next.path);
+        } else {
+            history.pushState(document.title, document.title, location.pathname + '#' + next.path);
+        }
+
+        this._container.innerHTML = html;
+        (next.controller.onNavigateTo || noop).call(next);
+    }
+
+    Router.prototype.eventHandler = function (e) {
+        this.go(new URL(e.newURL), new URL(e.oldURL), true);
+    }
+
+    Router.prototype.run = function () {
+        window.addEventListener('hashchange', this.eventHandler.bind(this), false);
+        this.go(window.location);
+    }
+
+    Router.prototype.destroy = function () {
+        window.removeEventListener('hashchange', this.eventHandler);
+    }
+
+    window.Router = Router;
+})(window);
+
+
+
+
+
